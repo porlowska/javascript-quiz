e18441c90973627e91d9bde6c4691c94322e912d
Polly <147508946+porlowska@users.noreply.github.com>
2023-12-05 13:58:59 +0000

    added starter code with pseudocode

---
+++ assets/js/logic.js
@@ -0,0 +1,52 @@
+// loop trouth the questions, with if satements so if time is less than 0 it exits the loop and it takes us to iniatals total socre and submit and then it takes us to highsocre html
+
+let button = document.getElementById("start") //or querySelector
+let timerEl = document.getElementById("time")
+let timer = 75
+button.addEventListener("click", start);
+
+function start(){
+//hide start screen id="start-screen" change class form start to hidden 
+//start question screens visible id="questions" class change to visible form hide (remove the classs attriblute or set to diferrent  )
+//start timer
+//
+};
+
+
+function timer(){
+
+    // set the timer 
+    //and start it
+}
+
+//for (var i=0, i < questions.length, i++){
+
+//}
+
+// section 1
+// start button that .eventListener(click, fuct)
+// HIDE START SCREEN
+// 1. tiggers the timer 
+// 2. starts first question
+
+//secton 2
+//Display queation form the array of questions (only if time > 0)
+// display 4 answers as buttons
+//if correct is clicket - feedbackCORRECT soud CORRECT and point up and next question 
+// if incorrect - feedback WRONG,sound WRONG, timer minus seconds 5or 10, next question, nopoint added,
+
+// section 3
+//if Last qaution or timer<=0
+//stop clearthe timer, 
+// count/display  the points/score final
+// triger to input initials INPUT AND BUTTon
+//submit page to beredirected to highscores // redirect to html page window object
+//save initialsandscores in local storage
+
+//section 4
+//High scores
+// sores higierst to lowest 
+// saved inlocal storege
+//page connected with main page to jump in between 
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+// qestions for the quiz - an array of objescts with answers
+
+let questions = [
+    {
+        title: "What is JS?",
+        answers:["JavaScript", "Java", "Pyton", "C++"],
+        correct: "JavaScript", //compare string values in logic.js
+    },{
+        title: "What is symbol of strict equality?",
+        answers: ["=","==","===","!=="],
+        correct: "==="
+    }
+
+]
+
